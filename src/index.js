6696f8209d98a78e1bbc450920b98a99afd78109
PuchkovSasha <Itworkpuchkov@yandex.ru>
2019-10-14 21:47:59 +0300

    feat:  add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+
+
+
+  let fact = expression.split("*");
+
+
+
+  let Five = 0;
+  let two = 0;
+
+  for (let index = 0; index < fact.length; index+=1){
+    
+    if (fact[index].includes('!!')) {
+      
+      
+      let counter = fact[index].slice(0, -2);
+
+      
+
+      
+      for (let j = counter; j > 0; j -= 2) {
+        
+        let num = j;
+        while (num % 5 == 0) {
+          
+          num /= 5;
+          
+          Five++;
+        }
+        while (num % 2 == 0) {
+          
+          num /= 2;
+          
+          two++;
+        }
+        continue;
+      }
+    } else {
+      let counter = fact[index].slice(0, -1);
+
+      for (let j = counter; j > 0; j-=1) {
+        let num = j;
+        while (num % 5 == 0) {
+          num /= 5;
+          Five++;
+        }
+        while (num % 2 == 0) {
+          num /= 2;
+          two++;
+        }
+        continue;
+      }
+    }
+ }
+
+
+  if (Five < two  ) {
+    return Five;
+  } else {
+    return two;
+  }
+ 
+};
+ 
